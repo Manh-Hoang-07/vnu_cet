b98dccb16a4517c385c5ec9260e8aa30334008e8
ManhHoang <vumanhhoang140799@gmail.com>
2021-02-21 15:35:12 +0700

    delete

--- routes/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:admin')->group(function (){
-    Route::get('/home','App\Http\Controllers\Admin\InfomationController@home')->name('admin.home');
-    Route::get('/logout-admin','App\Http\Controllers\Home\LoginController@logout_admin')->name('admin.logout');
-});
